9ccf8063b72ffd76d84ac18a76934942b366657b
Jordan Needham <jdncreations@hotmail.com>
2025-05-28 16:46:56 +1200

    add non-ok logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 const PORT = 8080;
 
+app.use(middlewareLogResponses);
 app.use('/app', express.static('./src/app'));
 
 function handlerReadiness(req: Request, res: Response) {
   res.set('content-type', 'text/plain; charset=utf-8');
   res.send('OK');
+}
+
+function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.on('finish', () => {
+    if (res.statusCode >= 400) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+    }
+  });
+  next();
 }
 
 app.get('/healthz', handlerReadiness);
