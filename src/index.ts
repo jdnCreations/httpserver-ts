93327f7fa4060e750550f4186aa72aef97f04f35
Jordan Needham <jdncreations@hotmail.com>
2025-05-30 15:20:38 +1200

    add custom error types and error handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { config } from './config.js';
-import { middlewareLogResponses, middlewareMetricsInc } from './middleware.js';
+import {
+  middlewareHandleErrors,
+  middlewareLogResponses,
+  middlewareMetricsInc,
+} from './middleware.js';
 import { replaceProfaneWords } from './utils/profanity.js';
+import { BadRequestError, NotFoundError } from './errors.js';
 
 const app = express();
 const PORT = 8080;
@@ -42,7 +47,7 @@ function handlerValidateChirp(req: Request, res: Response) {
   const params: parameters = req.body;
 
   if (params.body.length > 140) {
-    res.status(400).send({ error: 'Chirp is too long' });
+    throw new BadRequestError('Chirp is too long. Max length is 140');
   } else {
     const replaced = replaceProfaneWords(params.body);
     res.status(200).send({ cleanedBody: replaced });
@@ -50,12 +55,22 @@ function handlerValidateChirp(req: Request, res: Response) {
 }
 
 // API
-app.get('/api/healthz', handlerReadiness);
-app.post('/api/validate_chirp', handlerValidateChirp);
+app.get('/api/healthz', (req, res, next) => {
+  Promise.resolve(handlerReadiness(req, res)).catch(next);
+});
+app.post('/api/validate_chirp', (req, res, next) => {
+  Promise.resolve(handlerValidateChirp(req, res)).catch(next);
+});
 
 // ADMIN
-app.post('/admin/reset', handlerResetMetrics);
-app.get('/admin/metrics', handlerAdminMetrics);
+app.post('/admin/reset', (req, res, next) => {
+  Promise.resolve(handlerResetMetrics(req, res)).catch(next);
+});
+app.get('/admin/metrics', (req, res, next) => {
+  Promise.resolve(handlerAdminMetrics(req, res)).catch(next);
+});
+
+app.use(middlewareHandleErrors);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { config } from './config.js';
+import {
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+} from './errors.js';
 
 export function middlewareMetricsInc(
   req: Request,
@@ -23,4 +29,25 @@ export function middlewareLogResponses(
     }
   });
   next();
+}
+
+export function middlewareHandleErrors(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err instanceof BadRequestError) {
+    res.status(400).json({ error: err.message });
+  } else if (err instanceof UnauthorizedError) {
+    res.status(401).json({ error: err.message });
+  } else if (err instanceof ForbiddenError) {
+    res.status(403).json({ error: err.message });
+  } else if (err instanceof NotFoundError) {
+    res.status(404).json({ error: err.message });
+  } else {
+    res.status(500).json({
+      error: 'Something went wrong on our end',
+    });
+  }
 }
